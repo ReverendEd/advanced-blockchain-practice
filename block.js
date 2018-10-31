5be62dfd4721f45b10b348a4e07ac8a1d9ca2bc3
TylerPrivateGithub <eddywilkins34@gmail.com>
2018-10-31 11:09:12 -0500

    added mineBlock method, added jest and testing file

--- block.js
+++ block.js
@@ -1,3 +1,6 @@
+const SHA256 = require('crypto-js/sha256');
+
+
 class Block {
     constructor(timestamp, lastHash, hash, data){
         this.timestamp = timestamp,
@@ -19,6 +22,17 @@ class Block {
         return new this('Genesis time', '-----', 'first-hash', [])
     }
 
+    static mineBlock(lastBlock, data){
+        const timestamp = Date.now();
+        const lastHash = lastBlock.hash;
+        const hash = Block.hash(timestamp, lastHash, data);
+
+        return new this(timestamp, lastHash, hash, data)
+    }
+
+    static hash(timestamp, lastHash, data){
+        return SHA256(`${timestamp}${lastHash}${data}`).toString();
+    }
 
 }
 
