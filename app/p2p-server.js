cb50af9ed3cf9c2e8b26291388946792d395ee1e
TylerPrivateGithub <eddywilkins34@gmail.com>
2018-11-29 14:40:06 -0600

    finished project

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -4,7 +4,8 @@ const peers = process.env.PEERS ? process.env.PEERS.split(',') : [];
 
 const MESSAGE_TYPES = {
     chain: 'CHAIN',
-    transaction: 'TRANSACTION'
+    transaction: 'TRANSACTION',
+    clear_transactions: 'CLEAR_TRANSACTIONS'
 }
 
 class P2pServer {
@@ -45,6 +46,9 @@ class P2pServer {
                 case MESSAGE_TYPES.transaction:
                     this.transactionPool.updateOrAddTransaction(data.transaction)
                 break;
+                case MESSAGE_TYPES.clear_transactions:
+                    this.transactionPool.clear();
+                break;
             }
         });
     }
@@ -69,6 +73,12 @@ class P2pServer {
 
     broadcastTransaction(transaction) {
         this.sockets.forEach(socket => this.sendTransaction(socket, transaction))
+    }
+
+    broadcastClearTransactions(){
+        this.sockets.forEach(socket=>socket.send(JSON.stringify({
+            type: MESSAGE_TYPES.clear_transactions
+        })))
     }
 
 }
--- wallet/transaction.js
+++ wallet/transaction.js
@@ -1,4 +1,5 @@
 const ChainUtil = require('../chain-util');
+const { MINING_REWARD } = require('../config')
 
 class Transaction {
     constructor(){
@@ -22,14 +23,22 @@ class Transaction {
         return this;
     }
 
+    static transactionWithOutputs(senderWallet, outputs){
+        const transaction = new this();
+        transaction.outputs.push(...outputs)
+        Transaction.signTransaction(transaction, senderWallet);
+        return transaction;
+    }
+
 
     static newTransaction(senderWallet, recipient, amount){
-        const transaction = new this();
+        
         if(amount > senderWallet.balance){
             console.log(`Amount: ${amount} exceeds balance.`);
             return;
         }
-        transaction.outputs.push(...[
+
+        return Transaction.transactionWithOutputs(senderWallet, [
             { 
                 amount: senderWallet.balance - amount ,
                 address: senderWallet.publicKey
@@ -39,9 +48,12 @@ class Transaction {
                 address: recipient
             }
         ])
-        Transaction.signTransaction(transaction, senderWallet);
-        //this.logTransaction(transaction)
-        return transaction;
+    }
+
+    static rewardTransaction(minerWallet, blockchainWallet){
+        return Transaction.transactionWithOutputs(blockchainWallet, [{
+            amount: MINING_REWARD, address: minerWallet.publicKey
+        }])
     }
 
     static logTransaction(transaction){
